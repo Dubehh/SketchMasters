6eb0eaa3557f5664bced7acaa36ecf294e71384e
Dubehh <eelcoeikelboom@hotmail.com>
2016-04-02 14:02:08 +0200

    Moved dialog files + applied code conventions

--- static_src/script/client/connection-client.js
+++ static_src/script/client/connection-client.js
@@ -4,12 +4,14 @@
 
 $(document).ready(function(){
 
-    const loginForm = $("#test"); /* ID Van de Form*/
-    const loginInput = $("#texttest"); /* ID Van de text input box*/
-    const loginError = $("#error"); /* ID van de div waarin een error kan worden gedisplayed */
+    const loginForm = $("#loginForm"); /* ID Van de Form*/
+    const loginInput = $("#loginTextBar"); /* ID Van de text input box*/
+    const loginError = $("#loginErrorDisplay"); /* ID van de div waarin een error kan worden gedisplayed */
     const userDisplay = $("#userList"); /* ID van de div waar de users gedisplayed worden. */
     const loginSubmit = $("#loginSub");
+    const MAX_CHARS = 15;
 
+    initializeDialog();
     /*
     * Wanneer de user een 'account' heeft aangemaakt wordt er een object aan hem gekoppeld.
     * Wanneer de user al bestaat dan wordt er een false callback gemaakt, anders een true.
@@ -37,27 +39,48 @@ $(document).ready(function(){
         return str === null || str.match(/^ *$/) !== null;
     }
 
+    /*
+    * Kijkt of de ingevoerde string karakters bevat die je niet wil hebben.
+    * Op deze manier voorkom je ook XSS scripting.
+    * */
+    function hasInvalidCharacters(str){
+        const banned = ["<",">","$","@","#","*","^",";"];
+        for(i = 0; i < banned.length; i++)
+            if(str.indexOf(banned[i]) != -1) return true;
+        return false;
+    }
 
-    loginSubmit.click(function(event){
-            event.preventDefault();
-            if(isEmptyOrSpaces(loginInput.val()))
-            {
-                loginError.html("Vul een gebruikersnaam in");
-            }
-        else {
-            socket.emit(Command.NEW_USER, loginInput.val(), function(callback){
-                if(callback.NewUser){
-                    if(callback.User == null) return;
-                        loginForm.dialog('close');
+
+    /*
+    *  Wordt aangeroepen wanneer de user probeert in te loggen.
+    *  Er wordt gekeken of de username al bestaat en of de invoer valide is.
+    *  Als dat zo is dan mag de gebruiker joinen.
+    * */
+    loginSubmit.click(function(event) {
+        event.preventDefault();
+        var username = loginInput.val();
+        if (isEmptyOrSpaces(username)) {
+            displayError("Please fill in a Username.");
+        } else if(hasInvalidCharacters(username)){
+            displayError("Your username contains invalid characters.")
+        } else if(username.length > MAX_CHARS){
+            displayError("Your username length is limited to "+MAX_CHARS+" characters.");
+        } else {
+            socket.emit(Command.NEW_USER, loginInput.val(), function (callback) {
+                if(!callback.NewUser){
+                    displayError("This username is already used.");
                 }else{
-                    //Laat error zien dat naam al in gebruik is
+                    if(callback.User == null) return;
+                    loginForm.dialog('close');
                 }
             });
-            loginInput.val('');
-        }}
-    );
+        }
+        loginInput.val('');
+    });
 
+    function displayError(Error){
 
+    }
 
     /*
     * Krijgt de lijst van alle users binnen wanneer deze wordt geupdate
@@ -79,8 +102,24 @@ $(document).ready(function(){
 
     });
 
-
-
-
-
+    /*
+     * Zet de configuratie van de dialog op
+     * */
+    function initializeDialog(){
+        loginForm.dialog({
+            modal: true,
+            draggable: false,
+            resizable: false,
+            position: {
+                my: "center",
+                at: "center",
+                of: window
+            },
+            show: 'blind',
+            hide: 'blind',
+            closeOnEscape: false,
+            width: 330,
+            dialogClass: 'ui-dialog-osx'
+        });
+    }
 });
